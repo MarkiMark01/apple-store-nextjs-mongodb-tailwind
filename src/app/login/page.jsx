ef05af6d9e65a35893ff6b5009822e1dec782d11
MarkiMark01 <markishgroup@gmail.com>
2024-10-04 19:14:47 +0300

    added markup and css to Login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,7 +5,37 @@ const Login =
   () => {
     return (
       <section className="min-h-screen w-full">
-        
+        <h1 className="font-semibold text-4xl text-center pt-16">
+          Login
+        </h1>
+        <form className="flex flex-col gap-4 items-center justify-center h-full max-w-md mx-auto mt-8">
+          <input
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+          />
+          <button type="submit">
+          Login
+          </button>
+          <div className="my-4 text-center text-gray-500">
+          or login with provider
+        </div>
+        <button
+        type="button"
+        //   onClick={() => signIn('google', {callbackUrl:'/'})}
+          className="flex gap-4 justify-center  bg-white 
+          text-black w-full rounded-3xl p-2 text-xl border border-gray-300">
+          <Image src={'/google.png'} alt={''} width={24} height={24} />
+          Login with google
+        </button>
+        <div className="text-center my-4 text-gray-500 border-t pt-4">
+        Not registered yet{' '}
+          <Link className="underline text-orange-500" href={'/signup'}>Sign Up here &raquo;</Link>
+        </div>
+        </form>
       </section>
     );
   };
