9c474debcc3b9ac61a8c8a46146125e93b609a6a
MarkiMark01 <markishgroup@gmail.com>
2024-10-07 12:17:27 +0300

    added to register/route bcrypt

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -3,14 +3,13 @@ import {model, models, Schema} from "mongoose";
 const UserSchema = new Schema({
   name: {type: String},
   email: {type: String, required: true, unique: true},
-  password: {
-    type: String,
-    required: true,
-    validate: pass => {
-      if (!pass?.length || pass.length < 6) {
-        throw new Error("Password must be at least 6 characters");
-      }
-    },
+  password: {type: String,
+    // required: true,
+    // validate: pass => {
+    //     if(!pass?.length || pass.length < 6){
+    //         new Error("Password must be at least 6 characters")
+    //     }
+    // }
   },
   image: {type: String},
 }, {timestamps: true});
