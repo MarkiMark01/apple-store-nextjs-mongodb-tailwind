0f45a2f2a79f7640d3b5ee63c976e398d3acd5dc
MarkiMark01 <markishgroup@gmail.com>
2024-10-08 11:11:10 +0300

    created api/product

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -1,1 +1,27 @@
+import mongoose from "mongoose";
+import { Product } from '../../models/Product'; 
 
+export async function GET(req) {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('MongoDB connected'); 
+
+    const products = await Product.find();
+    
+
+    return new Response(JSON.stringify(products), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+  } catch (error) {
+
+    console.error('Error fetching products:', error);
+
+
+    return new Response(JSON.stringify({ error: 'Failed to fetch products' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+}
