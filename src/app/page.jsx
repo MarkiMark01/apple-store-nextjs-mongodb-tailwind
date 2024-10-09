c37c07b44d5af99ed72b494df411c6cc0ec04c27
MarkiMark01 <markishgroup@gmail.com>
2024-10-09 11:36:12 +0300

    added OvalLoader to Home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -29,7 +29,7 @@
 //         products.map((product) => (
 //           <li
 //             key={product._id}
-//             className="border p-4 rounded-lg shadow-md transition-transform 
+//             className="border p-4 rounded-lg shadow-md transition-transform
 //             duration-200 hover:scale-105 cursor-pointer flex flex-col"
 //           >
 //             <Image
@@ -48,8 +48,8 @@
 //             <section className="flex items-center justify-between p-4 rounded-lg shadow-md mt-auto">
 //               <p className="text-2xl font-bold">${product.price}</p>
 //               <button
-//                 className="border px-4 py-2 font-semibold rounded-lg 
-//                 bg-black text-yellow-200 hover:bg-yellow-200 
+//                 className="border px-4 py-2 font-semibold rounded-lg
+//                 bg-black text-yellow-200 hover:bg-yellow-200
 //                 hover:text-black"
 //               >
 //                 Add to cart
@@ -64,76 +64,90 @@
 //   </section>
 //   );
 // }
-
-
-
-
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
-// import ProductModal from "../components/modal/ProductModal";
+
+import OvalLoader from "../components/loader/OvalLoader";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null); 
 
   useEffect(() => {
     async function fetchProducts() {
+      setIsLoading(true);
       try {
         const res = await fetch("/api/product");
+        if (!res.ok) {
+          throw new Error("Failed to fetch products");
+        }
         const data = await res.json();
         setProducts(data);
       } catch (error) {
-        console.error("Failed to fetch products:", error);
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     }
 
     fetchProducts();
   }, []);
 
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <OvalLoader />
+      </div>
+    );
+  }
 
+  if (error) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        Something went wrong: {error}
+      </div>
+    );
+  }
 
   return (
-    <section className="max-w-6xl mx-auto p-4">
+    <section className="max-w-6xl mx-auto h-screen p-4">
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <li
-              key={product._id}
-              onClick={() => openModal(product)}
-              className="border p-4 rounded-lg shadow-md transition-transform 
+        {products.map((product) => (
+          <li
+            key={product._id}
+            onClick={() => openModal(product)}
+            className="border p-4 rounded-lg shadow-md transition-transform 
               duration-200 hover:scale-105 cursor-pointer flex flex-col"
-            >
-              <Image
-                src={product.image}
-                alt={product.title}
-                width={400}
-                height={300}
-                className="object-cover rounded-md mb-4"
-              />
-              <div className="flex-grow">
-                <h2 className="text-2xl font-bold">
-                  {product.title} {product.model}
-                </h2>
-                <p className="text-lg">{product.colour}</p>
-              </div>
-              <section className="flex items-center justify-between p-4 rounded-lg shadow-md mt-auto">
-                <p className="text-2xl font-bold">${product.price}</p>
-                <button
+          >
+            <Image
+              src={product.image}
+              alt={product.title}
+              width={400}
+              height={300}
+              className="object-cover rounded-md mb-4"
+            />
+            <div className="flex-grow">
+              <h2 className="text-2xl font-bold">
+                {product.title} {product.model}
+              </h2>
+              <p className="text-lg">{product.colour}</p>
+            </div>
+            <section className="flex items-center justify-between p-4 rounded-lg shadow-md mt-auto">
+              <p className="text-2xl font-bold">
+                ${product.price}
+              </p>
+              <button
                 className="border px-4 py-2 font-semibold rounded-lg bg-black text-yellow-200 hover:bg-yellow-200 hover:text-black"
                 onClick={(e) => e.stopPropagation()}
               >
                 Add to cart
               </button>
-              </section>
-            </li>
-          ))
-        ) : (
-          <p>Loading products...</p>
-        )}
+            </section>
+          </li>
+        ))}
       </ul>
-
-
     </section>
   );
 }
