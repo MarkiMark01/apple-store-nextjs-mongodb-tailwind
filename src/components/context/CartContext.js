6f983fea0afaf78bf3460351746970af920aefa5
MarkiMark01 <markishgroup@gmail.com>
2024-10-09 20:30:47 +0300

    created CartContext

--- src/components/context/CartContext.js
+++ src/components/context/CartContext.js
@@ -1,27 +1,27 @@
-// "use client";
-// import React, { createContext, useContext, useState, useEffect } from "react";
+"use client";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
-// const CartContext = createContext();
+const CartContext = createContext();
 
-// export const CartProvider = ({ children }) => {
-//   const [cart, setCart] = useState([]);
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
 
-//   useEffect(() => {
-//     const fetchCartItems = async () => {
-//       try {
-//         const response = await fetch("/api/shopCart");
-//         if (!response.ok) {
-//           throw new Error("Failed to fetch cart items");
-//         }
-//         const data = await response.json();
-//         setCart(data);
-//       } catch (error) {
-//         console.error("Error fetching cart items:", error);
-//       }
-//     };
+  useEffect(() => {
+    const fetchCartItems = async () => {
+      try {
+        const response = await fetch("/api/shopCart");
+        if (!response.ok) {
+          throw new Error("Failed to fetch cart items");
+        }
+        const data = await response.json();
+        setCart(data);
+      } catch (error) {
+        console.error("Error fetching cart items:", error);
+      }
+    };
 
-//     fetchCartItems();
-//   }, []);
+    fetchCartItems();
+  }, []);
 
 //   const addToCart = async (newCartItem) => {
 //     try {
@@ -71,13 +71,13 @@
 //     }
 //   };
 
-//   return (
-//     <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
-//       {children}
-//     </CartContext.Provider>
-//   );
-// };
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
 
-// export const useCart = () => {
-//   return useContext(CartContext);
-// };
+export const useCart = () => {
+  return useContext(CartContext);
+};
