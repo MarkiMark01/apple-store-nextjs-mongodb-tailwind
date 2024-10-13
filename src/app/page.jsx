64e7c0af79a159a45fecc21376af72bfb91f7182
MarkiMark01 <markishgroup@gmail.com>
2024-10-13 20:45:35 +0300

    added css input search to Home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,9 @@
-
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import OvalLoader from "../components/loader/OvalLoader";
 import ProductModal from "../components/modal/ProductModal";
-import { useCart } from "../components/context/CartContext"; 
+import { useCart } from "../components/context/CartContext";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
@@ -12,8 +11,9 @@ export default function Home() {
   const [error, setError] = useState(null);
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [input, setInput] = useState('');
-  
+  const [input, setInput] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
   const { addToCart } = useCart();
 
   useEffect(() => {
@@ -26,6 +26,7 @@ export default function Home() {
         }
         const data = await res.json();
         setProducts(data);
+        setFilteredProducts(data);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -35,6 +36,17 @@ export default function Home() {
 
     fetchProducts();
   }, []);
+
+  // useEffect(() => {
+  //   if (input) {
+  //     const results = products.filter((product) =>
+  //       product.title.toLowerCase().includes(input.toLowerCase())
+  //     );
+  //     setFilteredProducts(results);
+  //   } else {
+  //     setFilteredProducts(products);
+  //   }
+  // }, [input, products]);
 
   const addItemsToCart = (product) => {
     addToCart({
@@ -48,7 +60,7 @@ export default function Home() {
       quantity: 1,
     });
   };
-  
+
   const openModal = (product) => {
     setSelectedProduct(product);
     setIsModalOpen(true);
@@ -59,9 +71,9 @@ export default function Home() {
     setSelectedProduct(null);
   };
 
-  const handleInput = (e) => {
-    setInput(e.target.value)
-  }
+  // const handleInput = (e) => {
+  //   setInput(e.target.value);
+  // };
 
   if (isLoading) {
     return (
@@ -81,19 +93,28 @@ export default function Home() {
 
   return (
     <section className="max-w-6xl mx-auto min-h-screen p-4">
-      <section>
-        <input 
-        type="text" 
-        value={input} 
-        onChange={handleInput}
-        placeholder="Search"
-        
+      <section className="flex items-center justify-around">
+        <input
+          type="text"
+          value={input}
+          onChange={handleInput}
+          placeholder="What are you looking to buy today?"
+          className="mb-4 w-1/2 max-w-3xl flex p-2 rounded-lg border 
+          border-gray-300 shadow-md focus:outline-none focus:ring-2 
+          focus:ring-gray-500"
         />
+        <button
+          type="button"
+          className="bg-gray-800 text-yellow-200 rounded-3xl p-2 text-xl 
+          hover:bg-gray-700 mb-4 w-24"
+        >
+          Filters
+        </button>
       </section>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <li
-            key={product._id} 
+            key={product._id}
             onClick={() => openModal(product)}
             className="border p-4 rounded-lg shadow-md transition-transform 
               duration-200 hover:scale-105 cursor-pointer flex flex-col"
@@ -118,8 +139,8 @@ export default function Home() {
               <button
                 className="border px-4 py-2 font-semibold rounded-lg bg-black text-yellow-200 hover:bg-yellow-200 hover:text-black"
                 onClick={(e) => {
-                  e.stopPropagation(); 
-                 addItemsToCart(product)
+                  e.stopPropagation();
+                  addItemsToCart(product);
                 }}
               >
                 Add to cart
@@ -132,8 +153,9 @@ export default function Home() {
         product={selectedProduct}
         isOpen={isModalOpen}
         onClose={closeModal}
-        addToCart={addToCart} 
+        addToCart={addToCart}
       />
     </section>
   );
 }
+
