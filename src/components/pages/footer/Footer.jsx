e34fb6fa8967aeea1c1251fb8fc3e482adc9064f
MarkiMark01 <markishgroup@gmail.com>
2024-10-14 09:12:25 +0300

    changed Footer page

--- src/components/pages/footer/Footer.jsx
+++ src/components/pages/footer/Footer.jsx
@@ -1,14 +1,11 @@
-const Footer =
-  () => {
-    return (
-      <footer className="bg-black1 h-12 text-white flex items-center 
-      justify-center text-xl">
-        &copy;
-        2024
-        All
-        rights
-        reserved
-      </footer>
-    );
-  };
+const Footer = () => {
+  return (
+    <footer
+      className="bg-black1 h-12 text-white flex items-center 
+      justify-center text-xl"
+    >
+      2024 &copy; by Markiyan Marych
+    </footer>
+  );
+};
 export default Footer;
