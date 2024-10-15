62047b42c0c083a28ccfb2260d8ba061606bf9e2
MarkiMark01 <markishgroup@gmail.com>
2024-10-15 21:25:14 +0300

    updated ThanksModal

--- src/components/modal/ThanksModal.jsx
+++ src/components/modal/ThanksModal.jsx
@@ -1,10 +1,31 @@
 "use client";
-import React, { useState } from "react";
+import Image from "next/image";
+import React from "react";
 
 const ThanksModal = ({ onClose }) => {
-  return <section>
     
-  </section>;
+  return (
+    <div
+      onClick={handleBackdropClick}
+    >
+      <div >
+        <div >
+          <Image
+           src={'/thanks'}
+           alt={"Gratitude"}
+           width={400}
+           height={300}
+           className="object-cover rounded-md mb-4"
+          />
+          <button
+            onClick={handleBackdropClick}
+          >
+            Ok
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ThanksModal;
