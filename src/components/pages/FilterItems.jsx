0a400eb68fc2e2d0f7e6569d8d6b1aa993c636b3
MarkiMark01 <markishgroup@gmail.com>
2024-10-17 13:15:00 +0300

    added FilterItems to Home page

--- src/components/pages/FilterItems.jsx
+++ src/components/pages/FilterItems.jsx
@@ -12,7 +12,59 @@ const FilterItems = ({
 }) => {
   return (
     <section>
-      
+      <section className="flex justify-between items-center my-2 mb-4">
+        <input
+          type="text"
+          value={input}
+          onChange={handleInput}
+          placeholder="What are you looking to buy today?"
+          className="w-full p-3 rounded-lg border 
+    border-gray-300 shadow-md focus:outline-none focus:ring-2 
+    focus:ring-gray-500 transition duration-300 ease-in-out transform 
+    hover:scale-105"
+        />
+        <button
+          onClick={togglePriceSlider}
+          className="bg-gray-900 text-yellow-200 rounded-md 
+         w-40 sm:w-40 h-12 ml-4 flex items-center 
+         justify-center sm:justify-between px-4 text-lg 
+         transition duration-300 hover:bg-yellow-200 
+         hover:text-gray-900 shadow-lg transform 
+         hover:scale-105"
+        >
+          <CircleIcon className="w-3 h-3 hidden sm:inline" />
+          Price
+          <span className="hidden sm:inline">Slider</span>
+        </button>
+      </section>
+      {isSliderVisible && (
+        <section className="mb-6">
+          <h3 className="text-lg font-semibold mb-2">
+            Price Range: ${priceRange[0]} - ${priceRange[1]}
+          </h3>
+          <Range
+            step={1}
+            min={0}
+            max={1500}
+            values={priceRange}
+            onChange={(values) => setPriceRange(values)}
+            renderTrack={({ props, children }) => (
+              <div
+                {...props}
+                className="h-2 w-full bg-gray-300 rounded-lg"
+              >
+                {children}
+              </div>
+            )}
+            renderThumb={({ props }) => (
+              <div
+                {...props}
+                className="h-4 w-4 bg-black rounded-full cursor-pointer"
+              />
+            )}
+          />
+        </section>
+      )}
     </section>
   );
 };
