20e0760091a3c124cee1d44df409df1fbdd9c204
MarkiMark01 <markishgroup@gmail.com>
2024-10-18 13:13:46 +0300

    added PaymentCardNumber to Payment page

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+
+import PaymentCardNumber from "../../components/pages/payment/PaymentCardNumber";
 import ThanksModal from "../../components/modal/ThanksModal";
 import { useCart } from "../../components/context/CartContext";
 
@@ -31,8 +33,11 @@ const PaymentPage = () => {
     setErrorMessage("");
     openModal();
     clearCart();
-    router.push("/");
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
   };
+  
 
   const handleToBack = () => {
     router.push("/cart");
@@ -62,8 +67,10 @@ const PaymentPage = () => {
       setCardHolder("");
       setExpiryDate("");
       setCvc("");
+      setErrorMessage("");
     }
   }, [isModalOpen]);
+  
 
   return (
     <div className="max-w-4xl mx-auto min-h-screen p-4">
@@ -81,35 +88,12 @@ const PaymentPage = () => {
         {errorMessage && (
           <p className="text-red-500">{errorMessage}</p>
         )}
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="cardNumber"
-          >
-            Card Number
-          </label>
-          <input
-            type="text"
-            id="cardNumber"
-            placeholder="0000 0000 0000 0000"
-            value={cardNumber}
-            onChange={(e) => {
-              const value = e.target.value.replace(
-                /\D/g,
-                ""
-              );
-              if (value.length <= 16) {
-                setCardNumber(
-                  value.replace(/(.{4})/g, "$1 ").trim() 
-                );
-              }
-            }}
-            onBlur={handleCardNumberBlur}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            maxLength={19}
-            required
-          />
-        </div>
+        <PaymentCardNumber
+          cardNumber={cardNumber}
+          setCardNumber={setCardNumber}
+          handleCardNumberBlur={handleCardNumberBlur}
+        />
+
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
--- src/components/pages/payment/PaymentCardNumber.jsx
+++ src/components/pages/payment/PaymentCardNumber.jsx
@@ -1,8 +1,35 @@
-
-
-const PaymentCardNumber = () => {
-return (
-    <></>
-)
-}
+const PaymentCardNumber = ({
+  cardNumber,
+  setCardNumber,
+  handleCardNumberBlur,
+}) => {
+  return (
+    <div className="mb-4">
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="cardNumber"
+      >
+        Card Number
+      </label>
+      <input
+        type="text"
+        id="cardNumber"
+        placeholder="0000 0000 0000 0000"
+        value={cardNumber}
+        onChange={(e) => {
+          const value = e.target.value.replace(/\D/g, "");
+          if (value.length <= 16) {
+            setCardNumber(
+              value.replace(/(.{4})/g, "$1 ").trim()
+            );
+          }
+        }}
+        onBlur={handleCardNumberBlur}
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        maxLength={19}
+        required
+      />
+    </div>
+  );
+};
 export default PaymentCardNumber;
