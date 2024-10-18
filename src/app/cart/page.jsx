dde1efa01cae948b0f0f845a5adbb25c181a3d2d
MarkiMark01 <markishgroup@gmail.com>
2024-10-18 13:02:00 +0300

    added CartEmptyItems to Cart page

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-import CartIcon from "../../components/icons/CartIcon";
 import { useCart } from "../../components/context/CartContext";
 import CartGetItems from "../../components/pages/cart/CartGetItems";
 import CartBuyItems from "../../components/pages/cart/CartBuyItems";
+import CartEmptyItems from "../../components/pages/cart/CartEmptyItems";
 
 const Cart = () => {
   const { cart, removeFromCart } = useCart();
@@ -16,22 +16,15 @@ const Cart = () => {
   return (
     <section className="max-w-6xl mx-auto min-h-screen p-4">
       {cart.length === 0 ? (
-        <div className="text-xl font-bold flex items-center justify-center min-h-screen gap-4">
-          <p className="text-2xl font-semibold">
-            Your cart is empty...
-          </p>
-          <CartIcon />
-        </div>
+        <CartEmptyItems />
       ) : (
-        <CartGetItems 
-        removeFromCart={removeFromCart}
-        cart={cart}
+        <CartGetItems
+          removeFromCart={removeFromCart}
+          cart={cart}
         />
       )}
       {cart.length > 0 && (
-        <CartBuyItems
-        totalAmount={totalAmount}
-        />
+        <CartBuyItems totalAmount={totalAmount} />
       )}
     </section>
   );
--- src/components/pages/cart/CartEmptyItems.jsx
+++ src/components/pages/cart/CartEmptyItems.jsx
@@ -1,8 +1,13 @@
-
+import CartIcon from "../../icons/CartIcon";
 
 const CartEmptyItems = () => {
-return(
-    <></>
-)
-}
+  return (
+    <div className="text-xl font-bold flex items-center justify-center min-h-screen gap-4">
+      <p className="text-2xl font-semibold">
+        Your cart is empty...
+      </p>
+      <CartIcon />
+    </div>
+  );
+};
 export default CartEmptyItems;
