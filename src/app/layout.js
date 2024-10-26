7a64b364a39b93c90645825be826cb198aed7162
MarkiMark01 <markishgroup@gmail.com>
2024-10-26 12:44:04 +0300

    added new favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body>
         <AppProvider>
           <CartProvider>
